7cda2bc4c65a01c1bc15729fc49caa082a587cb6
carlos arosemena <ceag68@hotmail.com>
2017-08-22 21:53:29 -0400

    how to swap variables

---
+++ Setting and Swapping/SettingAndSwapping.js
@@ -0,0 +1,10 @@
+//Basic Swapping for Algorithms
+
+var myNumber = 42;
+var myName = 'carlos';
+var temp = myNumber;
+myNumber = myName;
+myName = temp;
+
+console.log(myNumber);
+console.log(myName);
