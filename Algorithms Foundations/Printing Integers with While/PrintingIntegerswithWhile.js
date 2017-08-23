94288b2f4cd82c5ac1b03661d4bb2de7f40f7dbe
carlos arosemena <ceag68@hotmail.com>
2017-08-22 22:16:04 -0400

    Integers While

---
+++ Algorithms Foundations/Printing Integers with While/PrintingIntegerswithWhile.js
@@ -0,0 +1,10 @@
+// Printing Integers with While
+// Print integers from 2000 to 5280, using a WHILE.
+
+
+var integer = 2000;
+
+while(integer >= 2000 && integer < 5281){
+  console.log(integer);
+  integer++;
+}
