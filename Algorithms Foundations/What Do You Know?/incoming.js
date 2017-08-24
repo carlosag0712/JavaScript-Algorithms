e057e01410e10fc8af441c9603743f01c311bfaa
carlos arosemena <ceag68@hotmail.com>
2017-08-24 18:09:11 -0400

    incoming

---
+++ Algorithms Foundations/What Do You Know?/incoming.js
@@ -0,0 +1,8 @@
+// What Do You Know?
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+function incoming(input){
+  console.log(input);
+}
+
+incoming('carlos');
