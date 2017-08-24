f80941533b304ae684a5b10f61de0def57959415
carlos arosemena <ceag68@hotmail.com>
2017-08-24 15:08:57 -0400

    printandcount

---
+++ Algorithms Foundations/Print and Count/PrintandCount.js
@@ -0,0 +1,27 @@
+// Print and Count
+// Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
+
+function printNCount(min,max){
+
+if(min == undefined){
+  min = 0;
+}
+
+if(max == undefined){
+  max = 5000;
+}
+
+  var count = 0;
+
+  if(min < max){
+    for(var i=min;i<=max;i++){
+      if(i%5==0){
+        count++;
+        console.log(i);
+      }
+    }
+  }
+  console.log('there were '+count+' numbers in the range of '+min+' and '+max);
+}
+
+printNCount()
